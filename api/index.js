d9deeacfa8035350feb0a46b8f4d8e4e8d22c699
MGalaCyber <ibmanggalaputra1274@gmail.com>
2025-03-20 09:16:55 +0700

    update

--- api/index.js
+++ api/index.js
@@ -7,21 +7,21 @@ const Ejs = require("ejs");
 require("dotenv").config();
 
 const app = Express();
+const port = process.env.PORT || 3001;
+
 app.set("trust proxy", 1);
 app.engine("html", Ejs.renderFile);
 app.set("view engine", "html");
 app.use(Express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "frame-ancestors *");
+    next();
+});
 app.use(Express.json());
 app.use(CookieParser());
 app.use(Cors());
 app.set("views", Path.join(__dirname, "../views"));
 app.use(Express.static(Path.join(__dirname, "../assets")));
-
-// Tambahkan CSP Header
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "frame-ancestors *");
-    next();
-});
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -31,5 +31,6 @@ app.use((req, res) => {
     res.redirect("/");
 });
 
-// Handler untuk Vercel API
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
